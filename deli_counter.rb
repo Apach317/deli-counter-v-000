3bf1ae5609eedd70c07ce6befdff2bea6fe033af
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-02 18:31:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif katz_deli.length > 0
     rich_deli = ["The line is currently:"]
-    katz_deli.each_with_index {|people, index| rich_deli << "#{index+1}. #{line}"}
+    katz_deli.each_with_index {|people, index| rich_deli << "#{index+1}. #{people}"}
     puts rich_deli.join(" ")
 
   end
