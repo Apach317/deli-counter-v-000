3856bdee697f435b9bfc8bf1f7e55df5f9533d23
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-02 18:25:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli == 0
+    rich_deli = "The line is currently:"
+  elsif katz_deli == []
+    return "The line is currently empty."
+  end
+end
