6910f9cc8ac61bc75bab8828e997ed771bf9684e
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-02 18:33:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
@@ -7,6 +5,20 @@ def line(katz_deli)
     rich_deli = ["The line is currently:"]
     katz_deli.each_with_index {|people, index| rich_deli << "#{index+1}. #{people}"}
     puts rich_deli.join(" ")
+  end
+end
 
+def take_a_number(katz_deli, people)
+  katz_deli << people
+  index = katz_deli.length
+  puts "Welcome, #{people}. You are number #{index} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts {"Currently serving " + katz_deli[0] + "."}
+    katz_deli.shift
   end
 end
